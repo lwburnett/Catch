6739a91ec0fa282e440f11634677753d6d076974
lephant33 <lephant33@gmail.com>
2019-01-13 10:06:43 -0700

    Created movement component
    - Created prototype (still empty) movement component
    
    - Updates to gitignore

---
+++ UE4/Source/Catch/Public/PrototypeMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PrototypeMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CATCH_API UPrototypeMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
